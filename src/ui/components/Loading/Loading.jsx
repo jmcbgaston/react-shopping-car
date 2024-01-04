e456504e76948b744fcaf2335047ab084bac035d
Jose Gaston <jmcbgaston@gmail.com>
2024-01-04 13:23:06 -0800

    componentize Loading

---
+++ src/ui/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+// TODO: Replace with skeleton and shimmer
+// Suspense + Boundary
+export default function Loading() {
+  return (
+    <div className="loading">
+      <p>Loading...</p>
+    </div>
+  );
+}
